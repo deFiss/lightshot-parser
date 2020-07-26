1e57e589f5d6b84b1f55ed4de750fea79f786487
deFiss <defiss@protonmail.com>
2020-07-26 23:10:14 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,31 @@
+# lightshot-parser
+
+lightshot-parser is a multi-process script for automatically downloading screenshots from https://prnt.sc, written in python
+
+
+### Setup
+
+```sh
+# Clone this repository (or just download and unzip the zip)
+$ git clone https://github.com/deFiss/lightshot-parser.git
+
+# Go to directory
+$ cd lightshot-parser
+
+# Install dependencies (if "pip" is missing, try using "pip3")
+$ sudo pip install -r requirements.txt
+```
+
+### Launch
+
+```sh
+$ python lightshot_pasher.py [process count]
+
+# or
+
+$ python lightshot_pasher.py
+
+# to enter the number of processes manually
+```
+
+With a large number of processes or over time, you may be banned by ip
---
+++ lightshot_pasher.py
@@ -0,0 +1,77 @@
+import requests
+import random
+from bs4 import BeautifulSoup
+import colorama
+from colorama import Fore as col
+import os
+import multiprocessing
+import sys
+
+
+def parse():
+    symbol_list = list('abcdefghiklmnopqrstvxyz123456789')
+
+    session = requests.Session()
+    session.headers.update({
+        'user-agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:78.0) Gecko/20100101 Firefox/78.0'
+    })
+
+    while True:
+        token = ''
+        for i in range(0, random.randint(3,8)):
+            token += random.choice(symbol_list)
+
+        resp = session.get('https://prnt.sc/{}'.format(token))
+
+        with open('res.html', 'w') as file:
+            file.write(resp.text)
+        
+        parsher = BeautifulSoup(resp.text, "html.parser")
+
+        image_url = parsher.find('meta', attrs= {'property': 'og:image'})['content']
+
+        if 'http' not in image_url:
+            print(f'{col.RED}[+] {col.LIGHTGREEN_EX}Nothing found by token {col.BLUE}{token}')
+            continue 
+        
+        print(f'{col.YELLOW}[+] {col.LIGHTGREEN_EX}Image found for token {col.BLUE}{token}{col.LIGHTGREEN_EX}. Download...')
+
+        img = session.get(image_url)
+        with open(os.path.join('output', token + image_url[-4:]), 'wb') as file:
+            file.write(img.content)
+        
+def main():
+    banner =  '''
+    ___       __    __       __          __                                       
+   / (_)___ _/ /_  / /______/ /_  ____  / /_      ____  ____ ______________  _____
+  / / / __ `/ __ \/ __/ ___/ __ \/ __ \/ __/_____/ __ \/ __ `/ ___/ ___/ _ \/ ___/
+ / / / /_/ / / / / /_(__  ) / / / /_/ / /_/_____/ /_/ / /_/ / /  (__  )  __/ /    
+/_/_/\__, /_/ /_/\__/____/_/ /_/\____/\__/     / .___/\__,_/_/  /____/\___/_/     
+    /____/                                    /_/                                 
+
+        {}https://github.com/deFiss/lightshot-parser
+    '''.format(col.LIGHTBLUE_EX)
+
+    print(col.CYAN+banner)
+    try:
+        os.mkdir('output')
+    except FileExistsError:
+        pass
+    
+    colorama.init()
+
+    if len(sys.argv) == 2:
+        proc_count = sys.argv[1]
+    else:
+        proc_count = input(f'{col.MAGENTA}[+] {col.YELLOW}Enter the number of processes: {col.LIGHTGREEN_EX}')
+        
+    process_list = []
+
+    for i in range(0, int(proc_count)):
+        proc = multiprocessing.Process(target=parse)
+        process_list.append(proc)
+        proc.start()
+        print(f'{col.YELLOW}[+] {col.LIGHTGREEN_EX}Process {col.BLUE}{i+1} {col.LIGHTGREEN_EX}start')
+
+if __name__ == "__main__":
+    main()
